79fc543bc03933e5b6438debada7f19a84b6fccc
j-development-projects <147048594+j-development-projects@users.noreply.github.com>
2023-10-15 12:55:42 +0100

    first commit

---
+++ app/Services/EventLogger/Base.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services\EventLogger;
+
+class Base
+{
+    public function calculate($n1, $n2)
+    {
+        return $n1 + $n2;
+    }
+}
---
+++ tests/Unit/EventLoggerServiceTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+// Service
+use App\Services\EventLogger\Base;
+
+class EventLoggerServiceTest extends TestCase
+{
+
+    protected $serviceBaseClass;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->serviceBaseClass = new Base();
+    }
+
+    public function test_calculation(): void
+    {
+        $result = $this->serviceBaseClass->calculate(5, 10);
+        $this->assertEquals(25, $result);
+    }
+}
