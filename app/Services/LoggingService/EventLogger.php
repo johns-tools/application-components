02ac958d0f93d8573897a9bc4ce20ddef2bf068f
j-development-projects <147048594+j-development-projects@users.noreply.github.com>
2023-10-17 04:36:21 +0100

    Checks Class added
    Added a checks class to clean up the code, and to allow for more details doc blocks (docs soon!).

--- app/Services/LoggingService/EventLogger.php
+++ app/Services/LoggingService/EventLogger.php
@@ -2,11 +2,11 @@
 
 namespace App\Services\LoggingService;
 
-// Utilities
+// Traits
 use App\Services\LoggingService\Traits\Utilities as EventLoggerUtilities;
 
-// Framework
-use Exception;
+// Support Classes
+use App\Services\LoggingService\Class\Checks as EventLoggerChecks;
 
 class EventLogger
 {
@@ -25,26 +25,13 @@ class EventLogger
 
     public function __construct(String $identifier, $storageDriver, Array $fileMeta)
     {
-        // Check we have the required meta data, that will be used to create the file name.
-        if(!isset($fileMeta['file_name']) or !isset($fileMeta['file_extension']))
-        {
-            throw new Exception("Missing required data from fileMeta.");
-        }
-
-        // Check an identifier is passed in, will be used when unique identity is required.
-        if(!$identifier)
-        {
-            throw new Exception("Missing required parameter `identifier`.");
-        }
-
-        $this->identifier = $identifier;
+        // Checks
+        EventLoggerChecks::checkFileMetaArray($fileMeta);
+        EventLoggerChecks::checkFileMetaArray($identifier);
 
         // After checking, we can now set the required variables local to this class instance.
-        if($storageDriver)
-        {
-           $this->storageDriver = $storageDriver;
-        }
-
+        $this->identifier = $identifier;
+        $this->storageDriver = $storageDriver ?? $this->storageDriver;
         $this->fileName = $fileMeta['file_name'];
         $this->fileExtension = $fileMeta['file_extension'];
 
